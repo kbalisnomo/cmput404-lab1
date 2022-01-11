9c44964fb9ccad89d1e9f07028653eeafba54cbd
kbalisnomo <kylephil@ualberta.ca>
2022-01-10 17:39:44 -0700

    Update lab1.py to GET itself from GitHub and print

--- lab1.py
+++ lab1.py
@@ -4,6 +4,9 @@ import requests
 print("Requests version: " + requests.__version__)
 
 # GET the Google homepage
-response = requests.get("http://google.com/")
-print("GET Google homepage: " + response.status_code)
+homepage = requests.get("http://google.com/")
+print("GET Google homepage: " + str(homepage.status_code))
 
+# GET this script from GitHub
+script = requests.get("https://raw.githubusercontent.com/kbalisnomo/cmput404-lab1/main/lab1.py")
+print("GET this script off GitHub:\n", script.text)
